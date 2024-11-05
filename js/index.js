0c5ede2f0d85e0d0ca5b00c610914075701eaf34
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-11-05 00:05:30 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,14 +10,46 @@ const term_output = document
 const bt_test = document
 .getElementById("bt-test")
 
-function test_input()
+const TEST_INPUT = 0
+const USER_INPUT = 1
+
+function gen_ouput(mode, test_input_text="", is_error=false, is_alert=false)
 {
     const user_input = term_input.value;
-    const new_span = document.createElement('span');
-    new_span.textContent = user_input;
-    term_output.appendChild(new_span);
+    const div = document.createElement('div');
+    const span1 = document.createElement('span');
+    const span2 = document.createElement('span');
+    span1.textContent = "testing@html-term:"
+    span1.classList.add("term-green");
+    if(is_error){
+        span2.classList.add("term-dark-red");
+    }
+    else if(is_alert){
+        span2.classList.add("term-yellow")
+    }
+    else{
+    span2.classList.add("term-white");
+    }
+    div.appendChild(span1);
+    div.appendChild(span2);
+    term_output.appendChild(div);
+    switch (mode)
+    {
+        case TEST_INPUT:
+            span2.textContent = ` $ ${test_input_text}`;
+            break;
+            
+            case USER_INPUT:
+                span2.textContent = ` $ ${user_input}`;
+        default:
+            break;
+    }
 }
 
+gen_ouput(TEST_INPUT, "Default text")
+gen_ouput(TEST_INPUT, "Alert text", false, true)
+gen_ouput(TEST_INPUT, "Error text", true)
+
 bt_test.addEventListener("click", ()=>{
-    test_input();
+    gen_ouput(USER_INPUT);
 })
